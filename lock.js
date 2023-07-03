f5b98a0e7c7c645e178b972ce2fea0aa2d016927
ZKBeat <51268863+ZKBeat@users.noreply.github.com>
2023-07-03 16:30:55 +0300

    Add files via upload

---
+++ lock.js
@@ -0,0 +1,65 @@
+var z = 0;
+function lock_f() {
+z=z+1;
+var pass = ["123", "324"];
+var sumb = [];
+for (var n = 0; n < Object.keys(pass).length; n++) {
+
+    for (var i = 0; i < pass[n].length; i++) {
+    sumb[i] = pass[n].charAt(i);
+    
+}
+if(z==3)
+{
+if (document.getElementById('num_'+ sumb[0]).checked){
+if (document.getElementById('num_'+ sumb[1]).checked) {
+if (document.getElementById('num_'+ sumb[2]).checked){
+alert("Поздравляю, вы приглашены на лофт! Ваше приглашение в беседу: https://vk.me/join/AJQ1d2FTNyiUqjE4W0O4m_kj . Чтобы получить скидку 100р., напишите в чат: Иди к нам! Стоимость 1700");
+}}}
+else
+alert("access denied");
+location.reload()
+}
+}
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // конечная дата, например 1 июля 2021
+    const deadline = new Date(2023, 6, 6);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+      $days.textContent = days < 10 ? '0' + days : days;
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+      $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+      $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+      $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+    // получаем элементы, содержащие компоненты даты
+    const $days = document.querySelector('.timer__days');
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
